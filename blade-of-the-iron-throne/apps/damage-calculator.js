d45ed49f9c6c734faf651ceb4a3876aac9fad6a8
bmacgregor <bmacgregor@users.noreply.github.com>
2016-09-12 14:24:49 -0600

    Update damage-calculator.js

--- blade-of-the-iron-throne/apps/damage-calculator.js
+++ blade-of-the-iron-throne/apps/damage-calculator.js
@@ -80,6 +80,75 @@ function getDamage(array){
 	return sendBack;
 }
 
+function getFeedback(result){
+	var feedback;
+	
+	if (result.Shock > 0){
+		feedback = "<strong>Shock: </strong>Shock is only applied once for each wound, but deducts dice from all " +
+		"three dice pools (Melee, Archery, and Sorcery) the moment the Character is struck by the blow. If the " +
+		"received Shock exceeds a Character’s total current Melee Pool for his very next action, the remainder of " +
+		"the Shock penalty is instead added to the aggressor's Melee Pool for the next Combat Round or Exchange. " +
+		"Such bonus dice are only usable against the Shocked opponent.<br/><br/>Shock is cumulative as long as it " +
+		"is applied to different zones. Should one attack the same area multiple times, the highest shock penalty " +
+		"— new or old — is applied (or re-applied) to the wounded party.<br/><br/>";
+	}
+	if (result.Pain > 0){
+		feedback += "<strong>Pain: </strong>Pain is deducted from each of the three pools at the beginning of each " +
+		"Combat Round, starting with the one following the Combat Round in which Shock from the wound was applied. " +
+		"Pain is reduced by one’s Tenacity. For the purposes of wound healing, where Pain is important, record the " +
+		"unreduced Pain rating of every wound, but apply only Pain in excess of the character’s Tenacity as a " +
+		"penalty to her pools.<br/><br/>Tenacity is applied in full to Pain with every single wound; do not total " +
+		"up Pain from several wounds first and then apply the Tenacity score. Instead, apply Tenacity to every " +
+		"single wound and then add up the reduced Pain ratings to determine the penalty to die pools.<br/><br/>Pain " +
+		"is cumulative as long as it is applied to different zones. Should one attack the same area multiple times, " +
+		"the highest shock penalty — new or old — is applied (or re-applied) to the wounded party.<br/><br/>";
+	}
+	if (result.BloodLoss > 0){
+		feedback += "<strong>Blood Loss: </strong>Blood Loss ratings of all wounds sustained are cumulative " +
+		"provided they come from different parts of the body. Each limb, the entire torso, and the head each count " +
+		"as individual locations. If a body part already sports a wound with an attendant Blood Loss rating and is " +
+		"wounded again in a way to indicate further Blood Loss, only the higher of the two ratings comes (or stays) " +
+		"in effect. At the very end of the Limelight in which the Character first acquired Blood Loss, and at the " +
+		"end of each subsequent Limelight, she must check Brawn. This Check must yield Successes equal to or " +
+		"surpassing the current Blood Loss number. If it doesn’t, the Character suffers a two die penalty to her " +
+		"Melee, Archery, and Spell Pools. In addition, her Attribute and Skill Checks will need an additional " +
+		"Success per full four dice penalty. The Brawn Check to avoid further Blood Loss is exempt from this " +
+		"requirement for additional Successes. These penalties represent dizziness or weakness from loss of blood, " +
+		"and accumulate from failed Brawn Check to failed Brawn Check.<br/><br/><br/><br/>Once accumulated " +
+		"penalties from failed Blood Loss Checks equal or exceed the character’s Brawn, the Character passes out " +
+		"from blood loss, and will likely die without quick medical attention. Thereafter, a Blood Loss Check is " +
+		"made for the unconscious Character at the end of each subsequent Round of Limelights. If any Check fails, " +
+		"the Character has bled to death. Blood Loss Checks made while the bleeding Character is at rest (no more " +
+		"strenuous activity than walking very slowly) require one less Success than indicated by the current Blood " +
+		"Loss rating to be successful. This includes Characters' unconscious from Blood Loss.";
+	}
+	if (result.Knockdown > 0){
+		feedback += "<strong>Knockdown: </strong>Knockdown can be the result of some wounds, especially (but not " +
+		"exclusively) those to the legs. With such wounds, the description will include a Knockdown number which " +
+		"denotes the number of Successes needed on a Knockdown Check to avoid falling prone. The Challenge Rating " +
+		"of this Check will be set at Challenging (two successes) if the Character’s Melee Pool is reduced to " +
+		"below –5, or simply Average (one success) in other cases. If the wound which caused the initial Shock " +
+		"reduced the Melee Pool to zero or below and also calls for a Check, only the more difficult of the two " +
+		"Checks need be made, but the Challenge Rating of the Check is increased by 1.<br/><br/>A prone Character " +
+		"can use only half of their Melee Pool while fighting from the ground. The number of dice used is halved " +
+		"after all other modifiers (Shock, Pain, Blood Loss, etc.) have been applied. If he is knocked down during " +
+		"an ongoing Combat Round, he immediately loses a number of dice from his Melee Pool equal to the " +
+		"difference between his Melee Pool at the beginning of the Round and half that number.<br/><br/>";
+	}
+	if (result.Knockout > 0){
+		feedback += "<strong>Knockout: </strong>Knockout can be a consequence of blows to the head. If the " +
+		"description of a wound contains a Knockout number, this is the Challenge Level the wounded Character needs " +
+		"to pass on an immediate Check of her Knockout Attribute. Failure on this check indicates unconsciousness. " +
+		"Optionally, any blow to the head that fails to cause a wound will still call for a Knockout Check " +
+		"nonetheless, unless the hit was received from a thrust Piercing or thrust Cleaving attack.<br/><br/>The " +
+		"Challenge Level of this Check is Average (one success) if a swung Piercing attack, Challenging (two " +
+		"successes) if a swung Cleaving or thrusting Blunt attack, and Difficult (three successes) if a swung Blunt " +
+		"attack, but may be under no circumstances higher than half the attack’s Quality of Success.";
+	}
+	
+	return feedback;
+}
+
 function damage(area, level, d6, shock, pain, bloodloss, knockdown, knockout, desc, location, gender, fatal){
 	this.Area = area;
 	this.Level = level;
@@ -122,6 +191,10 @@ function updateDamage(){
 	document.getElementById("Knockout").innerHTML = foundDamage.Knockout;
 	document.getElementById("Desc").innerHTML = "<strong>Description: </strong><em>" + foundDamage.Desc + "</em>";
 	document.getElementById("Location").innerHTML = "<strong>Location: </strong>" + foundDamage.Location;
+	
+	var feedbackString = getFeedback(foundDamage);
+	
+	document.getElementById("feedback").innerHTML = feedbackString;
 	
 	if (foundDamage.Fatal){document.getElementById("Desc").style.color = "red";}
 	else{document.getElementById("Desc").style.color = "black";}
