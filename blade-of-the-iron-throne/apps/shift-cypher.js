546f393a64b413d308715a0439e45ff7fd2abaf6
bmacgregor <bmacgregor@users.noreply.github.com>
2016-10-03 19:42:52 -0600

    Update shift-cypher.js

--- blade-of-the-iron-throne/apps/shift-cypher.js
+++ blade-of-the-iron-throne/apps/shift-cypher.js
@@ -4,14 +4,20 @@ function shift(text, n1, n2){
   var result = "";
   
   for (var i = 0; i < text.length; i++) {
-    var shift = n2;
+    //var shift = n2;
     
-    if (i % 2 === 0)  // if we're at an even index, use n1 shift value, else, use the n2 value
-      shift = n1;
+    //if (i % 2 === 0)  // if we're at an even index, use n1 shift value, else, use the n2 value
+      //shift = n1;
       
 		var c = text.charCodeAt(i);
-		if      (c >= 65 && c <=  90) result += String.fromCharCode((c - 65 + shift) % 26 + 65);  // Uppercase
-		else if (c >= 97 && c <= 122) result += String.fromCharCode((c - 97 + shift) % 26 + 97);  // Lowercase
+		if (c >= 65 && c <=  90){
+			var upperCaseNum = c - 65;
+			upperCaseNum += n1;
+			upperCaseNum %= 26;
+			upperCaseNum += 65;
+			result += String.fromCharCode(upperCaseNum);
+		}// Uppercase
+		else if (c >= 97 && c <= 122) result += String.fromCharCode((c - 97 + n1) % 26 + 97);  // Lowercase
 		else                          result += text.charAt(i);  // Copy
 	}
   
