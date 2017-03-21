462f2f17cf9dc57f1b69e48b80e1e8b8091f62f7
bmacgregor <bmacgregor@users.noreply.github.com>
2017-03-21 15:12:51 -0600

    Create die-roller.js

---
+++ DnD5e/die-roller.js
@@ -0,0 +1,49 @@
+<!-- hide script from old browsers
+
+var total = 0;
+var dX = 0;
+	
+function SetUpDisplay() {
+	document.getElementById("NUM_toHit").value = 0;
+	
+	DisplayRoll();
+}
+	
+function RollD20() {
+	var roll;
+		
+	total = 0;
+		
+	for (var i = 0; i < 4; i++) {
+		roll = Math.floor((Math.random() * 3) - 1);
+		
+		if (roll === 1) {
+			images[i] = "<img src='images/plus.bmp'></img>";
+			total++;
+		} else if (roll === -1) {
+			images[i] = "<img src='images/minus.bmp'></img>";
+			total--;
+		} else {images[i] = nothing;}		
+	}
+		
+	DisplayRoll();
+}
+	
+function DisplayRoll() {
+	document.getElementById("PAR_top").innerHTML = "";
+	document.getElementById("PAR_bottom").innerHTML = "";
+		
+	for (var i = 0; i < 4; i++) {
+		if (i === 0 || i === 1) {document.getElementById("PAR_top").innerHTML += images[i];}
+		else {document.getElementById("PAR_bottom").innerHTML += images[i];}
+	}
+		
+	UpdateTotal();
+}
+	
+function UpdateTotal(){
+	document.getElementById("PAR_total").innerHTML = "Total : " + 
+		(parseInt(document.getElementById("NUM_skill").value) + total);
+}
+
+// end hiding script from old browsers -->
