4938052d4347b38f77a05450960fe86756ca5e79
bmacgregor <bmacgregor@users.noreply.github.com>
2017-03-22 08:31:43 -0600

    Update die-roller.js

--- DnD5e/die-roller.js
+++ DnD5e/die-roller.js
@@ -19,6 +19,9 @@ function DisplayRoll() {
 	document.getElementById("DIV_total").innerHTML = "d20 Roll : " + 
 		(parseInt(document.getElementById("NUM_toHit").value) + d20Roll + "(" + d20Roll + " + " + 
 		document.getElementById("NUM_toHit").value + ")");
+	
+	if (d20Roll === 20)
+		document.getElementById("DIV_total").innerHTML += " Double Damage!";
 }
 
 // end hiding script from old browsers -->
