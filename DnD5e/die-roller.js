2090f3b30ebd47d5ccb66b5e343f4b08b53468ef
bmacgregor <bmacgregor@users.noreply.github.com>
2017-03-22 11:19:08 -0600

    Update die-roller.js

--- DnD5e/die-roller.js
+++ DnD5e/die-roller.js
@@ -29,23 +29,26 @@ function DisplayRoll() {
 	document.getElementById("DIV_total").innerHTML = "d20 Roll : " + 
 		(parseInt(document.getElementById("NUM_toHit").value) + d20Roll + " (" + d20Roll + " + " + 
 		document.getElementById("NUM_toHit").value + ")");
-	
-	if (d20Roll === 20) {
-		document.getElementById("DIV_total").innerHTML += " Double Damage!";
-	} else if (d20Roll === 1) {
-		document.getElementById("DIV_total").innerHTML += " Critical Failure!"; }
-	
-	if (document.getElementById("RDO_advantage").checked) {
-		document.getElementById("DIV_total").innerHTML += "<br/><br/>Rolled with Advantage (" + 
-		d20Roll + " vs " + d20Roll2 + ")";
-	} else if (document.getElementById("RDO_disadvantage").checked) {
-		document.getElementById("DIV_total").innerHTML += "<br/><br/>Rolled with Disadvantage (" + 
-		d20Roll + " vs " + d20Roll2 + ")"; }
+		
+	TellMeTheOdds();
 	
 	if (d20Roll !== 0) {
-	if (d20Roll + document.getElementById("NUM_toHit").value >= document.getElementById("NUM_targetAC").value) {
-		document.getElementById("DIV_total").innerHTML += "<br/><br/>Target hit!"; 
-	} else { document.getElementById("DIV_total").innerHTML += "<br/><br/>Target missed!"; } }
+		if (d20Roll === 20) {
+			document.getElementById("DIV_total").innerHTML += " Double Damage!";
+		} else if (d20Roll === 1) {
+			document.getElementById("DIV_total").innerHTML += " Critical Failure!"; }
+
+		if (document.getElementById("RDO_advantage").checked) {
+			document.getElementById("DIV_total").innerHTML += "<br/><br/>Rolled with Advantage (" + 
+			d20Roll + " vs " + d20Roll2 + ")";
+		} else if (document.getElementById("RDO_disadvantage").checked) {
+			document.getElementById("DIV_total").innerHTML += "<br/><br/>Rolled with Disadvantage (" + 
+			d20Roll + " vs " + d20Roll2 + ")"; }
+
+		if (d20Roll + document.getElementById("NUM_toHit").value >= document.getElementById("NUM_targetAC").value) {
+			document.getElementById("DIV_total").innerHTML += "<br/><br/>Target hit!"; 
+		} else { document.getElementById("DIV_total").innerHTML += "<br/><br/>Target missed!"; }
+	}
 }
 
 function TellMeTheOdds() {
