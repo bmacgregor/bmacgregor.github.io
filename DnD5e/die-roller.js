a2a12a72c66ba9c8fd44b0f43db4b79f12569893
bmacgregor <bmacgregor@users.noreply.github.com>
2017-03-22 08:33:49 -0600

    Update die-roller.js

--- DnD5e/die-roller.js
+++ DnD5e/die-roller.js
@@ -3,19 +3,19 @@
 var d20Roll = 0;
 
 function SetUpDisplay() {
-	console.log(Setting up page...);
+	console.log("Setting up page...");
 	document.getElementById("NUM_toHit").value = 0;
 	DisplayRoll();
 }
 
 function Rolld20() {
-	console.log(Rolling d20);
+	console.log("Rolling d20");
 	d20Roll = Math.floor(Math.random() * 20 + 1);
 	DisplayRoll();
 }
 
 function DisplayRoll() {
-	console.log(Displaying roll);
+	console.log("Displaying roll");
 	document.getElementById("DIV_total").innerHTML = "d20 Roll : " + 
 		(parseInt(document.getElementById("NUM_toHit").value) + d20Roll + "(" + d20Roll + " + " + 
 		document.getElementById("NUM_toHit").value + ")");
