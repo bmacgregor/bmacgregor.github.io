bc432ec4d8c414f8a892307ece52e12b466715a7
bmacgregor <bmacgregor@users.noreply.github.com>
2017-03-22 15:18:30 -0600

    Update die-roller.js

--- DnD5e/die-roller.js
+++ DnD5e/die-roller.js
@@ -65,11 +65,11 @@ function TellMeTheOdds() {
 }
 
 function GetDamage() {
-	var dType = parseInt(document.getElementById("SEL_damage").value);
-	var dMod = parseInt(document.getElementById("NUM_damage").value);
-	var dCount = parseInt(document.getElementById("NUM_dice").value);
+	var dType = document.getElementById("SEL_damage").value;
+	var dMod = document.getElementById("NUM_damage").value;
+	var dCount = document.getElementById("NUM_dice").value;
 	
-	var damage = Math.random * dType;
+	var damage = Math.random * parseFloat(dType);
 	damage += 1;
 	damage = Math.floor(damage);
 	damage += dMod;
