6983061f64d715aa87ec7f5e18d7735ac91d74fa
bmacgregor <bmacgregor@users.noreply.github.com>
2017-03-22 09:13:19 -0600

    Update die-roller.js

--- DnD5e/die-roller.js
+++ DnD5e/die-roller.js
@@ -12,10 +12,10 @@ function Rolld20() {
 	d20Roll = Math.floor(Math.random() * 20 + 1);
 	d20Roll2 = Math.floor(Math.random() * 20 + 1);
 	
-	if (document.getElementById("d20RollType").value === 1 && d20Roll2 > d20Roll
-		d20Roll = d20Roll2
-	else if (document.getElementById("d20RollType").value === -1 && d20Roll2 < d20Roll
-		d20Roll = d20Roll2
+	if (document.getElementById("d20RollType").value === 1 && d20Roll2 > d20Roll) {
+		d20Roll = d20Roll2;
+	} else if (document.getElementById("d20RollType").value === -1 && d20Roll2 < d20Roll) {
+		d20Roll = d20Roll2; }
 	
 	DisplayRoll();
 }
@@ -25,15 +25,15 @@ function DisplayRoll() {
 		(parseInt(document.getElementById("NUM_toHit").value) + d20Roll + " (" + d20Roll + " + " + 
 		document.getElementById("NUM_toHit").value + ")");
 	
-	if (d20Roll === 20)
+	if (d20Roll === 20) {
 		document.getElementById("DIV_total").innerHTML += " Double Damage!";
-	else if (d20Roll === 1)
-		document.getElementById("DIV_total").innerHTML += " Critical Failure!";
+	} else if (d20Roll === 1) {
+		document.getElementById("DIV_total").innerHTML += " Critical Failure!"; }
 	
-	if (document.getElementById("d20RollType").value === 1)
+	if (document.getElementById("d20RollType").value === 1) {
 		document.getElementById("DIV_total").innerHTML += "<br/>Rolled with Advantage";
-	else if (document.getElementById("d20RollType").value === -1)
-		document.getElementById("DIV_total").innerHTML += "<br/>Rolled with Disadvantage";
+	} else if (document.getElementById("d20RollType").value === -1) {
+		document.getElementById("DIV_total").innerHTML += "<br/>Rolled with Disadvantage"; }
 }
 
 // end hiding script from old browsers -->
