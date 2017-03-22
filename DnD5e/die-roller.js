8402f7d3d75d762d3df7781d3243c0a30f19acfb
bmacgregor <bmacgregor@users.noreply.github.com>
2017-03-22 08:37:19 -0600

    Update die-roller.js

--- DnD5e/die-roller.js
+++ DnD5e/die-roller.js
@@ -3,25 +3,24 @@
 var d20Roll = 0;
 
 function SetUpDisplay() {
-	console.log("Setting up page...");
 	document.getElementById("NUM_toHit").value = 0;
 	DisplayRoll();
 }
 
 function Rolld20() {
-	console.log("Rolling d20");
 	d20Roll = Math.floor(Math.random() * 20 + 1);
 	DisplayRoll();
 }
 
 function DisplayRoll() {
-	console.log("Displaying roll");
 	document.getElementById("DIV_total").innerHTML = "d20 Roll : " + 
 		(parseInt(document.getElementById("NUM_toHit").value) + d20Roll + " (" + d20Roll + " + " + 
 		document.getElementById("NUM_toHit").value + ")");
 	
 	if (d20Roll === 20)
 		document.getElementById("DIV_total").innerHTML += " Double Damage!";
+	else if (d20Roll === 1)
+		document.getElementById("DIV_total").innerHTML += " Critical Failure!";
 }
 
 // end hiding script from old browsers -->
