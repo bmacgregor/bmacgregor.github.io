30f26a2d63a0be3281dcdf9b7e66538516845a6e
bmacgregor <bmacgregor@users.noreply.github.com>
2017-03-22 11:16:48 -0600

    Update die-roller.js

--- DnD5e/die-roller.js
+++ DnD5e/die-roller.js
@@ -42,9 +42,10 @@ function DisplayRoll() {
 		document.getElementById("DIV_total").innerHTML += "<br/><br/>Rolled with Disadvantage (" + 
 		d20Roll + " vs " + d20Roll2 + ")"; }
 	
+	if (d20Roll !== 0) {
 	if (d20Roll + document.getElementById("NUM_toHit").value >= document.getElementById("NUM_targetAC").value) {
 		document.getElementById("DIV_total").innerHTML += "<br/><br/>Target hit!"; 
-	} else { document.getElementById("DIV_total").innerHTML += "<br/><br/>Target missed!"; }
+	} else { document.getElementById("DIV_total").innerHTML += "<br/><br/>Target missed!"; } }
 }
 
 function TellMeTheOdds() {
