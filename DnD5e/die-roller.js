896cfb017359bd7851965e57ecd08d5038603f64
bmacgregor <bmacgregor@users.noreply.github.com>
2017-03-24 08:07:34 -0600

    Update die-roller.js

--- DnD5e/die-roller.js
+++ DnD5e/die-roller.js
@@ -85,7 +85,8 @@ function AverageDamage() {
 	var dMod = parseFloat(document.getElementById("NUM_damage").value);
 	var dCount = parseFloat(document.getElementById("NUM_dice").value);
 	
-	var avg = parseFloat((dCount + (dCount * dType) + (dCount + dMod)) / 2).toFixed(1);
+	var avg = (dCount + (dCount * dType) + (dCount + dMod)) / 2;
+	avg = avg.toFixed(2);
 	
 	document.getElementById("DIV_dAvg").innerHTML = "Average damage : " + avg;
 }
